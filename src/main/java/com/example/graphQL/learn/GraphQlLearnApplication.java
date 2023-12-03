0e2e966e65c05d31a130e78bc05e713b75c6bb85
savindu29 <1savindupanagoda@gmail.com>
2023-12-03 07:21:08 +0530

    first commit

---
+++ src/main/java/com/example/graphQL/learn/GraphQlLearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.graphQL.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQlLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/graphQL/learn/GraphQlLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graphQL.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
